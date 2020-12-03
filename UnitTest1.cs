e4d8de01b191387e323abdd187e2a17e3bcc1476
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2020-12-03 15:46:40 +0200

    update2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -22,7 +22,7 @@ namespace UnitTestProject1
                 TVector2D vec2d2 = new TVector2D(s2, g2);
 
                 TVector2D vec = new TVector2D(vec2d + vec2d2);
-                Assert.AreEqual(s + s2, vec.a);
+                Assert.AreEqual(s + s2, vec.x);
 
             }
             for (int i = 0; i < 20; i++)
@@ -36,8 +36,8 @@ namespace UnitTestProject1
                 TVector2D vec2d2 = new TVector2D(s2, g2);
 
                 TVector2D vec = new TVector2D(vec2d + vec2d2);
-                Assert.AreEqual(g + g2, vec.b);
-                Assert.AreEqual(s + s2, vec.a);
+                Assert.AreEqual(g + g2, vec.y);
+                Assert.AreEqual(s + s2, vec.x);
 
             }
             for (int i = 0; i < 20; i++)
@@ -55,24 +55,7 @@ namespace UnitTestProject1
                 Assert.AreEqual(vec2d * vec2d2, (s * s2) + (g * g2));
 
             }
-            for (int i = 0; i < 20; i++)
-            {
-                double s = r.Next(-1000, 1000);
-                double g = r.Next(-1000, 1000);
-                double z = r.Next(-1000, 1000);
-                TVector3D vec2d = new TVector3D(s, g, z);
-
-                double s2 = r.Next(-1000, 1000);
-                double g2 = r.Next(-1000, 1000);
-                double z2 = r.Next(-1000, 1000);
-                TVector3D vec2d2 = new TVector3D(s2, g2, z2);
-
-                double n = s * s2;
-                double m = g * g2;
-                double v = z * z2;
-                Assert.AreEqual(vec2d * vec2d2, n + m + v);
-
-            }
+            
             for (int i = 0; i < 20; i++)
             {
                 double s = r.Next(-1000, 1000);
@@ -85,10 +68,11 @@ namespace UnitTestProject1
                 double z2 = r.Next(-1000, 1000);
                 TVector3D vec2d2 = new TVector3D(s2, g2, z2);
                 TVector3D vec = new TVector3D(vec2d - vec2d2);
-
-                Assert.AreEqual(vec.a, s - s2);
-                Assert.AreEqual(vec.b, g - g2);
-                Assert.AreEqual(vec.c, z - z2);
+                Console.WriteLine(vec);
+                Console.WriteLine(s-s2);
+                Assert.AreEqual(vec.x, s - s2);
+                Assert.AreEqual(vec.y, g - g2);
+                Assert.AreEqual(vec.z, z - z2);
             }
         }
     }
